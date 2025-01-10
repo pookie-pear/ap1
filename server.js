1ba166002b4e56bb4bd0967dd964613bd6b8e0f9
pookie-pear <vestok33@gmail.com>
2025-01-10 05:58:00 -0500

    removed __dirname from serving static

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve static files (HTML, GLTF, JS, etc.) from the "public" folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.get("/",(err,res)=>
 {
