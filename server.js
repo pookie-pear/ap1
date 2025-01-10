2fee95cb4e4f06872da673ecf454e30d28e10cc7
pookie-pear <vestok33@gmail.com>
2025-01-10 06:58:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,11 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Catch 404 errors for static files
-app.use((req, res, next) => {
-  res.status(404).send('File not found');
-});
 
 // Serve the main HTML file
 app.get("/", function(req, res){
   res.send("igga");
-});
-
-// Global error handler
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send('Internal Server Error');
 });
 
 const port = process.env.PORT || 3000;
