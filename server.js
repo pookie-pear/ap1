a97f2987ea9738b2cf40f1e05559619f6ff29f90
pookie-pear <vestok33@gmail.com>
2025-01-10 06:08:02 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-// server.js
-
 const express = require('express');
 const path = require('path');
 const app = express();
-// Serve static files (HTML, GLTF, JS, etc.) from the "public" folder
-app.use(express.static(__dirname+'./public'));
 
-app.get("/",(err,res)=>
-{
-    res.sendFile("./Untitled-1.html")
-})
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Start the server
-const port = process.env.PORT || 3000;  // Use Render's PORT environment variable
+// Catch 404 errors for static files
+app.use((req, res, next) => {
+  res.status(404).send('File not found');
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('Internal Server Error');
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
